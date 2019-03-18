fa176fca553cbe3bae5063aa110c998796fb9ee4
Xavier Georget <xageorget@gmail.com>
2019-03-18 09:59:17 +0100

    add of the component sign-up

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SignUp } from '../sign-up';
+
+@Component({
+  selector: 'sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  firstSignUp : SignUp = {
+    email: "youremail@dresss.com"
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
