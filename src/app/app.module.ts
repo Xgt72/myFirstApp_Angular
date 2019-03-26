2d32d47dac4cf4d19ac7406cd2c64162aeffe852
Xavier Georget <xageorget@gmail.com>
2019-03-26 18:04:58 +0100

    add of menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -8,6 +9,10 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { HighLightCardDirective } from './high-light-card.directive';
 import { DisplayMovieListDirective } from './display-movie-list.directive';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ROUTES } from './app.routes';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,15 @@ import { DisplayMovieListDirective } from './display-movie-list.directive';
     UserProfileComponent,
     MovieCardComponent,
     HighLightCardDirective,
-    DisplayMovieListDirective
+    DisplayMovieListDirective,
+    HomeComponent,
+    ContactComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
